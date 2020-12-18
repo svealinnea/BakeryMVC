f2add555ce9de5a50a319237e59892b7cbcaf891
Svea Wade <svealinneawade@gmail.com>
2020-12-18 11:31:06 -0800

    test for returning title passes

--- Bakery.Tests/ModelTests/OrderTests.cs
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -11,8 +11,20 @@ namespace Bakery.Tests
     [TestMethod]
     public void OrderConstructor_CreateInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("", "",5, 5);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+    [TestMethod]
+    public void SetTitle_ReturnsTitle_String()
+    {
+      string title = "vendor order 1";
+      Order newOrder = new Order(title, "", 0, 0); 
+
+      string updatedTitle = "vendor order 2";
+      newOrder.Title = updatedTitle;
+      string result = newOrder.Title;
+
+      Assert.AreEqual(updatedTitle, result);
     }
   }
 }
--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -4,6 +4,17 @@ namespace Bakery.Models
 {
   public class Order
   {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public int Date { get; set; }
 
+  public Order(string title, string description, int price, int date)
+  {
+    Title = title;
+    Description = description;
+    Price = price; 
+    Date = date;
+  }
   }
 }
